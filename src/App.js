8877e90e1b98926362a2a0b60c2d4d7321303964
= <=>
2024-02-21 15:54:20 +0700

    vid 16 output list

--- src/App.js
+++ src/App.js
@@ -8,9 +8,18 @@ const App = () => {
   //state
   let [name, setName] = useState('Gấu Gấu');
   let [address, setAddress] = useState('');
+  let [todos, setTodos] = useState([
+    { id: 'todo1', name: 'Đi chợ' }, { id: 'todo2', name: 'Nấu cơm' }, { id: 3, name: 'Rửa bát' }, { id: 4, name: 'Học bài' }
+  ]);
 
   const handleEventClick = (event) => {
-    setName(address);
+    if (!address) {
+      alert('Please enter Todo name!');
+      return;
+    }
+    let newTodo = { id: 'abc', name: address };
+    setTodos([...todos, newTodo]);
+    setAddress('');
   }
 
   const handleChangeInput = (event) => {
@@ -22,9 +31,14 @@ const App = () => {
       <Nav></Nav>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello {name}
-        </p>
+        <h1>
+          Hello world with {name}
+        </h1>
+        <div className='todos-container'>
+          {todos.map((todo, index) => {
+            return <div key={todo.id}>{todo.name}</div>
+          })}
+        </div>
         <input type='text' value={address} onChange={(event) => handleChangeInput(event)}></input>
         <button type='button' onClick={(event) => handleEventClick(event)}>Click me</button>
       </header>
