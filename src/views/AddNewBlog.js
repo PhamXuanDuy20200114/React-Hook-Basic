9793865c332aebb4585bb66c4a5f2bbe83fb3926
= <=>
2024-03-04 22:57:19 +0700

    post request vid 36

--- src/views/AddNewBlog.js
+++ src/views/AddNewBlog.js
@@ -1,16 +1,27 @@
 import './Blog.scss';
 import { useState } from 'react';
+import axios from 'axios';
 
-const AddNewBlog = () => {
+const AddNewBlog = (props) => {
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
         if (!title || !content) {
             alert('Please fill all fields');
         }
 
+        let data = {
+            title: title,
+            body: content,
+            userId: 1
+        }
+        let res = await axios.post('https://jsonplaceholder.typicode.com/posts', data);
+        if (res && res.data) {
+            let newBlog = res.data;
+            props.handleAddNew(newBlog);
+        }
     }
     return (
         <div className="add-new-container">
@@ -28,6 +39,8 @@ const AddNewBlog = () => {
             </form>
         </div>
     );
+
+
 }
 
 export default AddNewBlog;
--- src/views/Blog.js
+++ src/views/Blog.js
@@ -2,27 +2,66 @@ import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import useFetch from "../customize/fetch";
 import "./Blog.scss";
 import { Link } from 'react-router-dom';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import { useEffect, useState } from "react";
+import AddNewBlog from "./AddNewBlog";
 
 const Blog = () => {
-    let history = useHistory();
-    const { data, isLoading } = useFetch('https://jsonplaceholder.typicode.com/posts/');
-    let newData = [];
-    if (data && data.length > 0) {
-        newData = data.slice(0, 9);
-        console.log('Blog data:', newData)
+    const [show, setShow] = useState(false);
+    const [newData, setNewData] = useState([]);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => {
+        setShow(true);
     }
 
-    const handleAddNew = () => {
-        history.push('/add-new-blog');
+    let history = useHistory();
+    const { data, isLoading } = useFetch('https://jsonplaceholder.typicode.com/posts/');
+    useEffect(() => {
+        if (data && data.length > 0) {
+            if (data && data.length > 0) {
+                let newDataSimple = data.slice(0, 9);
+                setNewData(newDataSimple);
+            }
+        }
+    }, [data]);
+
+    const handleAddNew = (blog) => {
+        let data = newData;
+        data.unshift(blog);
+
+        setShow(false);
+        setNewData(data);
+    }
+
+    const deletePost = (id) => {
+        let data = newData;
+        data = data.filter(item => item.id !== id)
+        setNewData(data);
     }
     return (
         <>
-            <button className="btn-add-new" onClick={handleAddNew}>+ Add new Blog</button>
+
+            <Button variant="primary" className="my-3" onClick={handleShow}>
+                Add new Blog
+            </Button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <AddNewBlog handleAddNew={handleAddNew} />
+                </Modal.Body>
+            </Modal>
+
             <div className="blog-container">
                 {isLoading === false && newData && newData.length > 0 && newData.map((item, index) => {
                     return (
                         <div className="single-blog" key={item.id}>
-                            <div className="title">{item.title}</div>
+                            <div className="title">
+                                <span>{item.title}</span> <span onClick={() => deletePost(item.id)}>X</span></div>
                             <div className="content">{item.body}</div>
                             <button>
 
@@ -39,3 +78,4 @@ const Blog = () => {
 }
 
 export default Blog;
+
